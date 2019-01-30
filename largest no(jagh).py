12083f1912419f4131e5d7b4ccccc423e8f40125
jaghadish281098 <47181280+jaghadish281098@users.noreply.github.com>
2019-01-30 19:09:40 +0530

    Add files via upload

---
+++ largest no(jagh).py
@@ -0,0 +1,9 @@
+n1=int(raw_input())
+n2=int(raw_input())
+n3=int(raw_input())
+if n1>n2 and n1>n3:
+    print "n1 is greater"
+elif n2>n3:
+    print "n2 is greater"
+else:
+    print "n3 is greater"
